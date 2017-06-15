370eff460edcbbe9dfd9e27e11b66fdde2b1ec11
abinstephen1989 <astephen@interac.ca>
2017-06-15 12:21:59 +0530

    Create IDGeneratorController.java
    Jenkin Build testing

--- src/main/java/tcs/ms/poc/idgenerator/IDGeneratorController.java
+++ src/main/java/tcs/ms/poc/idgenerator/IDGeneratorController.java
@@ -2,8 +2,6 @@ package tcs.ms.poc.idgenerator;
 
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
-
-
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.web.bind.annotation.GetMapping;
